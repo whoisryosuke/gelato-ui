69563d23f1a7d7dcb5c92004c4e727a96c7a2db3
Ryosuke <ryosuke.san.hana@gmail.com>
2022-01-23 00:15:19 -0800

    :zap: clsx added - more styles

--- libs/components/src/lib/button/button.css.ts
+++ libs/components/src/lib/button/button.css.ts
@@ -5,7 +5,11 @@ export const buttonStyles = style({
   display: 'flex',
   backgroundColor: themes.tokens.colors.primary,
   borderRadius: base.tokens.space[1],
+  border: 0,
 
+  ':disabled': {
+    backgroundColor: themes.tokens.colors.gray[500],
+  },
   ':hover': {
     backgroundColor: themes.tokens.colors.blue[400],
   },
--- libs/components/src/lib/button/button.tsx
+++ libs/components/src/lib/button/button.tsx
@@ -1,10 +1,18 @@
-import { buttonStyles } from './button.css';
+import { buttonStyles, sizeVariants } from './button.css';
+import clsx from 'clsx';
 
 /* eslint-disable-next-line */
-export interface ButtonProps {}
+export interface ButtonProps {
+  size?: keyof typeof sizeVariants;
+  children: React.ReactNode;
+}
 
-export function Button({ children }: React.PropsWithChildren<ButtonProps>) {
-  return <button className={buttonStyles}>{children}</button>;
+export function Button({ children, size = 'medium' }: ButtonProps) {
+  return (
+    <button className={clsx(buttonStyles, sizeVariants[size])}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
